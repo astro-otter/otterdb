417385409254bd9e164aab38512b04417503d4d9
Noah Franz <noahfranz13@gmail.com>
2024-01-19 15:38:53 -0500

    fix some bugs introduced by migration

--- data/scripts/import.sh
+++ data/scripts/import.sh
@@ -2,7 +2,7 @@
 
 ### IMPORT ########################
 # run arango import
-cd ../base
+cd $(dirname $(dirname $0))/base
 for f in *.json
 do
     arangoimport --file $f --server.database "otter" --collection "tdes" --server.username "admin@otter" --server.password "insecure" --define "key=name['default_name']"
--- db/builddb.js
+++ db/builddb.js
@@ -1,21 +1,21 @@
 const { db } = require("@arangodb");
 
 // create the database
-db._createDatabase("tide")
-db._useDatabase("tide")
+db._createDatabase("otter")
+db._useDatabase("otter")
 
 // create the tde collection
 db._create("tdes")
 
 // create our users
 const users = require('@arangodb/users');
-users.save('admin@tide', 'insecure'); // for full access
-users.save('user@tide', 'password'); // for the api
+users.save('admin@otter', 'insecure'); // for full access
+users.save('user@otter', 'insecure'); // for the api
 
 // give admin full access
-users.grantDatabase('admin@tide', 'tide', 'rw')
-users.grantCollection('admin@tide', 'tide', 'tdes', 'rw')
+users.grantDatabase('admin@otter', 'otter', 'rw')
+users.grantCollection('admin@otter', 'otter', 'tdes', 'rw')
 
 // give users only read access
-users.grantDatabase('user@tide', 'tide', 'ro')
-users.grantCollection('user@tide', 'tide', 'tdes', 'ro')
+users.grantDatabase('user@otter', 'otter', 'ro')
+users.grantCollection('user@otter', 'otter', 'tdes', 'ro')
