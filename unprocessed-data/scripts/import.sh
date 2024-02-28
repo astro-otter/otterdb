2c0e50ab0734f90aee1279c54a4374e9307dc179
Noah Franz <nfranz@arizona.edu>
2024-02-28 10:46:32 -0700

    reorganize a bit

--- unprocessed-data/scripts/import.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-### IMPORT ########################
-# run arango import
-for f in $1/*.json
-do
-    arangoimport --file $f --server.database "otter" --collection "tdes" --server.username "admin@otter" --server.password "insecure" --define "key=name['default_name']"
-done
